97c4b9100d1b3d43450429e7b64b8c27bec66a8c
pine951128 <pine951128@hanmail.net>
2021-02-05 14:36:22 +0900

    Recycler View

---
+++ app/src/main/java/com/mac_available/ex59recyclerview2/Item.java
@@ -0,0 +1,19 @@
+package com.mac_available.ex59recyclerview2;
+
+public class Item {
+    public String name;
+    public String msg;
+    public int icon;
+    public int img;
+
+    public Item(){
+
+    }
+
+    public Item(String name, String msg, int icon, int img){
+        this.name = name;
+        this.msg = msg;
+        this.icon = icon;
+        this.img = img;
+    }
+}
