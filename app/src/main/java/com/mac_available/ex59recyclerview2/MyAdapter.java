d3caee62f2750aba683ae6e7119ce6312077e256
Youngwoo1128 <77264918+Youngwoo1128@users.noreply.github.com>
2021-02-05 14:47:33 +0900

    Update MyAdapter.java

--- app/src/main/java/com/mac_available/ex59recyclerview2/MyAdapter.java
+++ app/src/main/java/com/mac_available/ex59recyclerview2/MyAdapter.java
@@ -23,8 +23,6 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MyAdapter extends RecyclerView.Adapter {
 
-    //이거도 push
-
     Context context;
     ArrayList<Item> items;
 
